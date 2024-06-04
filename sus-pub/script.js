6e916030c7e48a9aae661aec8409187f1f5e584e
Jake <95489310+Jakelolipopp@users.noreply.github.com>
2024-06-04 14:21:36 +0200

    update sus-pub/script.js

---
+++ sus-pub/script.js
@@ -0,0 +1,28 @@
+function handle(res) {
+    document.getElementById('chat-box').innerText = res.content;
+    unselectSend();
+        
+}
+
+function btnclck() {
+    send({messages: [{"role":"user", "content":document.getElementById('message-input').value}]});
+    document.getElementById('message-input').value = '';
+}
+
+function unselectSend() {
+    var element = document.getElementById('send-btn'); var clone = element.cloneNode(true); element.parentNode.replaceChild(clone, element);
+}
+
+
+async function send(data) {
+    const options = {
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    }   
+    const response = await fetch('/jb-ai', options);
+    const json = await response.json();
+    handle(json);
+}
